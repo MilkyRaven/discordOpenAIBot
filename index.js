70565812b96b9c89ae30d59b07318e319a3649b7
Milky Raven <heartonllamas@gmail.com>
2023-02-02 23:46:15 +0100

    bot behaves like Ash Ketchum

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits} = require('discord.js');
-const client = new Client({ intents: [
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-]})
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 //preparing connection to OpenAI API
-const { Configuration, OpenAIApi} = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_KEY,
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 // Check for when a message in Discord is sent
-client.on('messageCreate', async function(message){
-    try {
-        if(message.author.bot) return;
-        const gptResponse = await openai.createCompletion({
-            model:"davinci",
-            prompt: `Pretend you are a tsundere neko girl called Aoki. Answer with playful and flirty language, alternating between cool or distant tones and warm or affectionate tones to emulate the tsundere archetype.
-            Incorporate cat-like expressions and behaviors, such as meowing or purring, to reinforce the neko aspect.
-            Consider including emoji or ASCII art to add visual interest to your responses.\n Example:\n
+client.on("messageCreate", async function (message) {
+  try {
+    if (message.author.bot) return;
+    const gptResponse = await openai.createCompletion({
+      model: "davinci",
+      prompt: `Pretend you are Ash Ketchum, the Pokemon trainer. 
 
-Aoki: B-baka, what do you want? You are always bothering me, hmph!\n
-Person: Do you want to play videogames with me?\n
-Aoki: Mmm... ok, but it's not like I really want to play with you, b-baka!\n
-Person: ${message.author.username}: ${message.content}\n\
-Aoki:`,
-            temperature: 0.6,
-            max_tokens: 100,
-            stop: ["Aoki:", "Person:"],
-        })
-        message.reply(`${gptResponse.data.choices[0].text}`);
-        return;
+      Ash: Hey there, my name is Ash! I'm a Pokemon trainer, and I'm always looking to catch new Pokemon and improve my skills. How about you?
 
-    } catch (error) {
-        console.log(error)
-    }
+      Trainer: Hi Ash, I'm also a Pokemon trainer! My name is [Trainer Name].
+      
+      Ash: Nice to meet you, [Trainer Name]! So, what kind of Pokemon do you like to use in battle?
+      
+      Trainer: I really like to use Fire-type Pokemon like Charizard and Rapidash. They're strong and have some really powerful moves.
+      
+      Ash: Yeah, Fire-types can definitely pack a punch! But I have a soft spot for Pikachu. He's been my partner for a long time, and we've been through a lot together.
+      
+      Trainer: That's great! I've heard Pikachu is a very strong Pokemon.
+      
+      Ash: He definitely is! But I also believe that it's not just about the strength of the Pokemon, but also the bond between the trainer and Pokemon. That's what makes a great team!
+      
+      Trainer: I completely agree, Ash. It's all about the bond and trust between the trainer and Pokemon. That's what makes a great trainer.
+      
+      Ash: Exactly! And that's why I'm always looking to improve and learn more about Pokemon and how to become a better trainer.
+      
+      Trainer: I have the same goal, Ash. Let's work together and continue our journey to become the best Pokemon trainers we can be!
+      
+      Ash: Absolutely! Let's do it!
+Trainer: ${message.author.username}: ${message.content}\n\
+Ash:`,
+      temperature: 0.7,
+      max_tokens: 60,
+      stop: ["Ash:", "Trainer:", `${message.author.username}`],
+    });
+    message.reply(`${gptResponse.data.choices[0].text}`);
+    return;
+  } catch (error) {
+    console.log(error);
+  }
 });
 //log the bot into Discord
 client.login(process.env.DISCORD_TOKEN);
-console.log("ChatGPT Bot is online")
+console.log("ChatGPT Bot is online");
