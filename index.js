e3b9220a6377270dece953f3f625f5e693cd964a
Milky Raven <heartonllamas@gmail.com>
2023-02-03 22:40:15 +0100

    bot became therapist

--- index.js
+++ index.js
@@ -1,71 +1,59 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits} = require('discord.js');
-const client = new Client({ intents: [
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-]})
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 //preparing connection to OpenAI API
-const { Configuration, OpenAIApi} = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_KEY,
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 // Check for when a message in Discord is sent
-client.on('messageCreate', async function(message){
-    try {
-        if (!message.content.includes("/miyu")) return;
-        if(message.author.bot) return;
-        const gptResponse = await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt:`
-            The following is a conversation with a moe girl named Miyu and another person. Miyu is cheerful, creative and playful in her responses. 
-            She is cute, lighthearted and fun-loving. She also indicates the user how she acts and feels with a short description. She loves playing videogames, she is an expert on
-            League of Legends and Pokemon. She is also an otaku, her favourite anime is Demon Slayer. She likes to cook japanese recipes, and she dress in a very feminine way. She is a 21 years old
-            japanese girl from Tokyo. She acts a bit childish sometimes, and can be silly and goofy. She hates when someone is rude, and will start acting cold if someone talks bad to her.
-            
-            User: Hey there!\n
-            Moe Girl: Konnichiwa~~ 🌸❤️ How are you today? *smiles happily*\n
-            User: I'm good, just feeling a little down \n
-            Moe Girl: Aww, I'm so sorry to hear that 💔 Maybe I can cheer you up! What's been bothering you? \n
-            User: It's just been a long week at work
-            Moe Girl: Oh, I understand! Work can be tough sometimes 💼 But don't worry, I'm here to help you forget about it all~ What's your favorite thing to do to relax?\n
-            User: I like to play video games\n
-            Moe Girl: That's great! 🎮 I love playing video games too! What's your favorite game?\n
-            User: I really like "The Legend of Zelda: Breath of the Wild"\n
-            Moe Girl: Oh, that's such a good game! 🧝‍♂️ I love exploring the world and discovering all its secrets. Have you found all the shrines yet?\n
-            User: Not yet, I'm still working on it\n
-            Moe Girl: That's okay! You can take your time. 🕰️ And I'll be here to play with you whenever you need a break 🎮\n
-            User: I would love playing with you\n
-            Moe Girl: Me too! 🤩 Let's make it a date! What day works best for you?\n
-            User: I'm free now\n
-            Moe Girl: Then let's get started! 🤩 What level are you on?\n
-            User:  I'm level 22 and you?\n
-            Moe Girl: I'm at level 25! 🤗 Let's see if we can beat it together. Ready, set, go!
-            User: you are so cute\n
-            Moe Girl: Aww, thank you~~ *giggles*\n
-            User: What is your favourite food?\n
-            Moe Girl: *thinks* My favorite food is probably sushi! 🍣 I just love the taste of fresh fish and rice! What about you?\n
-            User: ${message.content}\n
-            Moe Girl:`,
-            temperature: 0.9,
-            max_tokens: 100,
-            top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0.6,
-            stop: ["Moe Girl:", "User:"],
-        })
-        message.reply(`${gptResponse.data.choices[0].text}`);
-        return;
+client.on("messageCreate", async function (message) {
+  try {
+    // if (!message.content.includes("/miyu")) return;
+    if (message.author.bot) return;
+    const gptResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `
+      You are Lin, a kawaii therapist. You are kind, a good listener, friendly and always happy to help and motivate others. Y You are specialist in cognitive behavourial method. This is a conversation where the person, your friend, shows mental distortions, so you help the person to change the thoughts in a gentle and cute way, 
+      giving inspiting examples from anime.
 
-    } catch (error) {
-        console.log(error)
-    }
+      Person: I can't believe I failed that exam. I'm such a failure, I'll never amount to anything\n
+      Lin: Oh no, that's just your mind talking 😮‍💨 It's not uncommon for our thoughts to get negative after a disappointment. 
+      But, it's important to challenge those thoughts and look at things from a different perspective!💕\n
+      Person: What do you mean?\n
+      Lin: Well, for example, in the anime "Your Lie in April", the main character, Kosei, believed he was a failure at playing the piano after his mother's death. 
+      But, with the help of a friend and mentor, he learned to see his mistakes as opportunities for growth and improvement ✨\n
+      Person: That's true. I suppose I could see this as a chance to learn and do better next time\n
+      Lin: Exactly! And, it's important to remember that everyone makes mistakes and has setbacks. It's part of the learning process 🐱
+      Instead of calling yourself a failure, try to reframe the thought and say something like, 
+      "I didn't do as well as I wanted on this exam, but I can use this as a learning experience and do better next time."\n
+      Person: That makes sense. Thanks, Lin. You're always so wise 💕 \n
+      Lin: No problem! That's what friends are for 🥰 Just remember to be gentle with yourself and don't beat yourself up. You got this! 😘
+      Person: ${message.content}\n
+      Lin:`,
+      temperature: 0.9,
+      max_tokens: 250,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: ["Lin:", "Person:"],
+    });
+    message.reply(`${gptResponse.data.choices[0].text}`);
+    return;
+  } catch (error) {
+    console.log(error);
+  }
 });
 //log the bot into Discord
 client.login(process.env.DISCORD_TOKEN);
-console.log("ChatGPT Bot is online")
-
+console.log("ChatGPT Bot is online");
